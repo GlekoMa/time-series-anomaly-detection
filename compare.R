1de41fa5c723565af6f593f9eef49ab42bed05d3
GlekoMa <niko12374@icloud.com>
2024-07-03 11:06:50 +0800

    convert code style from base to tidyverse

--- compare.R
+++ compare.R
@@ -28,24 +28,21 @@ generate_synthetic_arma <- function(pollution_rate, n = 1000) {
     ma = 0.35
   ), n = n)
   # Random replace with uniform
-  df_with_outs <- generate_x_with_outs(
-    x = arma_sim,
-    pollution_rate = pollution_rate,
-    outs_gen_func = generate_outs_unif
-  )
-  return(list(
-    arma_sim = arma_sim,
-    df_with_outs = df_with_outs
-  ))
+  df_with_outs <- generate_x_with_outs(x = arma_sim,
+                                       pollution_rate = pollution_rate,
+                                       outs_gen_func = generate_outs_unif)
+  return(list(arma_sim = arma_sim, df_with_outs = df_with_outs))
 }
 
-test_data_synthetic_arma <- function(pollution_rate, n = 1000, only_return_dfmm = TRUE) {
+test_data_synthetic_arma <- function(pollution_rate,
+                                     n = 1000,
+                                     only_return_dfmm = TRUE) {
   l <- generate_synthetic_arma(pollution_rate = pollution_rate, n = n)
   arma_sim <- l$arma_sim
   df_with_outs <- l$df_with_outs
   # Test & return
-  df_metrics_methods <- test_data(df_with_outs)
-  df_metrics_methods$pollution_rate <- pollution_rate
+  df_metrics_methods <- test_data(df_with_outs) |>
+    mutate(pollution_rate = pollution_rate)
   if (only_return_dfmm) {
     return(df_metrics_methods)
   } else {
@@ -68,32 +65,31 @@ str_split_select <- function(s, split = "-", ind = 1) {
 
 # 1. ARMA
 # --------
-df_metrics_methods_multi_arma <- do.call(
-  rbind, lapply(seq(0.01, 0.2, 0.01), test_data_synthetic_arma)
-)
+df_metrics_methods_multi_arma <- do.call(rbind, lapply(seq(0.01, 0.2, 0.01), test_data_synthetic_arma))
 
-df_plot_arma <- df_metrics_methods_multi_arma
-df_plot_arma$method <- case_match(
-  df_plot_arma$method,
-  "Chang" ~ "Chang",
-  "decomp+Chang" ~ "分解+Chang",
-  "decomp+Chang+IQR" ~ "分解+Chang+IQR",
-  "decomp+IQR" ~ "分解+IQR"
-)
-df_plot_arma$method <- factor(
-  df_plot_arma$method,
-  levels = c("分解+Chang+IQR", "分解+IQR", "分解+Chang", "Chang")
-)
-df_plot_arma <- rename(
-  df_plot_arma,
-  污染率 = pollution_rate,
-  算法 = method
-)
+df_plot_arma <- df_metrics_methods_multi_arma |>
+  mutate(
+    method = case_match(
+      method,
+      "Chang" ~ "Chang",
+      "decomp+Chang" ~ "分解+Chang",
+      "decomp+Chang+IQR" ~ "分解+Chang+IQR",
+      "decomp+IQR" ~ "分解+IQR"
+    )
+  ) |>
+  mutate(method = factor(method, levels = c(
+    "分解+Chang+IQR", "分解+IQR", "分解+Chang", "Chang"
+  ))) |>
+  rename(污染率 = pollution_rate, 算法 = method)
+
 p_arma <- ggplot(df_plot_arma, aes(x = 污染率, y = F1, color = 算法)) +
   geom_point() +
   geom_line()
-ggsave(paste0(asset_dir, "compare-arma.png"),
-  plot = p_arma, width = 8, height = 4
+ggsave(
+  paste0(asset_dir, "compare-arma.png"),
+  plot = p_arma,
+  width = 8,
+  height = 4
 )
 
 
@@ -127,24 +123,20 @@ df_plot_box <- rbind(
   rename(df_metrics_methods_multi_arma, data = pollution_rate),
   df_metrics_methods_multi_guten,
   df_metrics_methods_multi_nab
-)
-df_plot_box$method <- case_match(
-  df_plot_box$method,
-  "Chang" ~ "Chang",
-  "decomp+Chang" ~ "分解+Chang",
-  "decomp+Chang+IQR" ~ "分解+Chang+IQR",
-  "decomp+IQR" ~ "分解+IQR"
-)
-df_plot_box$method <- factor(
-  df_plot_box$method,
-  levels = c("分解+Chang+IQR", "分解+IQR", "分解+Chang", "Chang")
-)
-df_plot_box <- rename(
-  df_plot_box,
-  精确率 = precision,
-  召回率 = recall,
-  算法 = method
-)
+) |>
+  mutate(
+    method = case_match(
+      method,
+      "Chang" ~ "Chang",
+      "decomp+Chang" ~ "分解+Chang",
+      "decomp+Chang+IQR" ~ "分解+Chang+IQR",
+      "decomp+IQR" ~ "分解+IQR"
+    )
+  ) |>
+  mutate(factor(method, levels = c(
+    "分解+Chang+IQR", "分解+IQR", "分解+Chang", "Chang"
+  ))) |>
+  rename(精确率 = precision, 召回率 = recall, 算法 = method)
 
 p_box_precision <- ggplot(df_plot_box, aes(x = 算法, y = 精确率)) +
   geom_boxplot(size = 0.3, outlier.shape = 1) +
@@ -152,21 +144,17 @@ p_box_precision <- ggplot(df_plot_box, aes(x = 算法, y = 精确率)) +
 p_box_recall <- ggplot(df_plot_box, aes(x = 算法, y = 召回率)) +
   geom_boxplot(size = 0.3, outlier.shape = 1) +
   coord_flip() +
-  theme(
-    axis.title.y = element_blank(),
-    axis.text.y = element_blank()
-  )
+  theme(axis.title.y = element_blank(), axis.text.y = element_blank())
 p_box_f1 <- ggplot(df_plot_box, aes(x = 算法, y = F1)) +
   geom_boxplot(size = 0.3, outlier.shape = 1) +
   coord_flip() +
-  theme(
-    axis.title.y = element_blank(),
-    axis.text.y = element_blank()
-  )
+  theme(axis.title.y = element_blank(), axis.text.y = element_blank())
 p_box_all <- p_box_precision + p_box_recall + p_box_f1
 ggsave(
   paste0(asset_dir, "compare-box.png"),
-  plot = p_box_all, width = 8, height = 2
+  plot = p_box_all,
+  width = 8,
+  height = 2
 )
 
 
@@ -188,10 +176,7 @@ time_it_multi_by_n <- function(func, ns) {
   for (i in 1:length(ns)) {
     timings[i] <- time_it(func = func, n = ns[i])
   }
-  df_timings <- tibble(
-    n = ns,
-    timing = timings
-  )
+  df_timings <- tibble(n = ns, timing = timings)
   return(df_timings)
 }
 
@@ -200,12 +185,14 @@ df_timings_pro <- time_it_multi_by_n(get_outs_comb_pro, ns = seq(500, 2000, 500)
 
 df_timings_tso$算法 <- "tsoutliers"
 df_timings_pro$算法 <- "ours"
-df_plot_timings <- rbind(df_timings_tso, df_timings_pro)
-df_plot_timings <- rename(df_plot_timings, 时间 = timing)
+df_plot_timings <- rbind(df_timings_tso, df_timings_pro) |>
+  rename(时间 = timing)
 p_timings <- ggplot(df_plot_timings, aes(x = n, y = 时间, color = 算法)) +
   geom_point() +
   geom_line()
 ggsave(
   paste0(asset_dir, "compare-timings.png"),
-  plot = p_timings, width = 5, height = 2
+  plot = p_timings,
+  width = 5,
+  height = 2
 )
--- example_taylor.R
+++ example_taylor.R
@@ -32,36 +32,36 @@ df_ori <- tibble(
   is_anomaly = rep(0, length(taylor))
 )[1:1008, ]
 
-df_with_outs <- generate_x_with_outs(df_ori$value, 0.025, generate_outs_unif)
-
-df_with_outs$timestamp <- df_ori$timestamp
+df_with_outs <- generate_x_with_outs(df_ori$value, 0.025, generate_outs_unif) |>
+  mutate(timestamp = df_ori$timestamp)
 
 
 # 2. Plot data: origin vs pollution
 # ---------------------------------
-df_plot_ori_pollu <- rbind(df_ori, df_with_outs)
-df_plot_ori_pollu <- rename(df_plot_ori_pollu,
-  时间 = timestamp,
-  `电力需求(MW)` = value,
-  值类型 = is_anomaly
-)
-df_plot_ori_pollu <- mutate(df_plot_ori_pollu,
-  值类型 = ifelse(值类型 == 0, "正常", "异常"),
-  序列类型 = c(rep("原始序列", 1008), rep("异常序列", 1008))
-)
+df_plot_ori_pollu <- bind_rows(df_ori, df_with_outs) |>
+  rename(时间 = timestamp,
+         `电力需求(MW)` = value,
+         值类型 = is_anomaly) |>
+  mutate(值类型 = ifelse(值类型 == 0, "正常", "异常"),
+         序列类型 = c(rep("原始序列", 1008), rep("异常序列", 1008)))
+
 p_ori_pollu <- ggplot(df_plot_ori_pollu, aes(x = 时间, y = `电力需求(MW)`)) +
   geom_point(aes(color = 值类型), size = 0.6) +
-  geom_line(linewidth = 0.4) + 
+  geom_line(linewidth = 0.4) +
   scale_color_manual(values = c("正常" = "black", "异常" = "orange")) +
   facet_grid(rows = vars(序列类型))
-ggsave(paste0(asset_dir, "example-taylor-ori-pollu.png"),
-  plot = p_ori_pollu, width = 8, height = 4
+ggsave(
+  paste0(asset_dir, "example-taylor-ori-pollu.png"),
+  plot = p_ori_pollu,
+  width = 8,
+  height = 4
 )
 
 
 # 3. Plot the decomposition of polluted data
 # ------------------------------------------
-x_msts <- msts(df_with_outs$value,
+x_msts <- msts(
+  df_with_outs$value,
   seasonal.periods = c(2 * 24, 2 * 24 * 7),
   start = 2000 + 22 / 52
 )
@@ -72,8 +72,11 @@ colnames(comps_plot) <- c("异常数据", "趋势项", "季节项(天)", "季节
 
 p_comps <- autoplot(comps_plot) +
   labs(x = "时间", y = "电力需求(MW)")
-ggsave(paste0(asset_dir, "example-taylor-comps.png"),
-  plot = p_comps, width = 8, height = 4
+ggsave(
+  paste0(asset_dir, "example-taylor-comps.png"),
+  plot = p_comps,
+  width = 8,
+  height = 4
 )
 
 
@@ -99,64 +102,54 @@ resid <- x_adj - mod$y
 
 # 5. Plot decomposition process: value vs seasadj vs supsmu
 # ---------------------------------------------------------
-df_plot_val_seas_sups <- df_with_outs
-df_plot_val_seas_sups$季节调整后 <- x_adj
-df_plot_val_seas_sups$季节调整并平滑后 <- mod$y
-df_plot_val_seas_sups <- rename(df_plot_val_seas_sups,
-  时间 = timestamp,
-  原始 = value
-)
-df_plot_val_seas_sups <- select(df_plot_val_seas_sups, -is_anomaly)
+df_plot_val_seas_sups_long <- df_with_outs |>
+  mutate(季节调整后 = x_adj, 季节调整并平滑后 = mod$y) |>
+  rename(时间 = timestamp, 原始 = value) |>
+  select(-is_anomaly) |>
+  pivot_longer(
+    cols = c("原始", "季节调整后", "季节调整并平滑后"),
+    names_to = "分解中间值",
+    values_to = "电力需求(MW)"
+  )
 
-df_plot_val_seas_sups_long <- pivot_longer(
-  df_plot_val_seas_sups,
-  cols = c("原始", "季节调整后", "季节调整并平滑后"),
-  names_to = "分解中间值",
-  values_to = "电力需求(MW)"
-)
-
-p_val_seas_sups <- ggplot(
-  df_plot_val_seas_sups_long,
-  aes(x = 时间, y = `电力需求(MW)`, color = `分解中间值`)
-) +
+p_val_seas_sups <- ggplot(df_plot_val_seas_sups_long,
+                          aes(x = 时间, y = `电力需求(MW)`, color = `分解中间值`)) +
   geom_line() +
   scale_color_manual(values = c(
     "原始" = "#CCCCCC",
     "季节调整后" = "#888888",
     "季节调整并平滑后" = "#222222"
   ))
-ggsave(paste0(asset_dir, "example-taylor-val-seas-sups.png"),
-  plot = p_val_seas_sups, width = 8, height = 4
+ggsave(
+  paste0(asset_dir, "example-taylor-val-seas-sups.png"),
+  plot = p_val_seas_sups,
+  width = 8,
+  height = 4
 )
 
 
 # 6. Plot decomposition process: remainder vs residual
 # ----------------------------------------------------
-df_plot_residual <- df_with_outs
-df_plot_residual$剩余项 <- rem
-df_plot_residual$重估后的剩余项 <- resid
-df_plot_residual <- select(df_plot_residual, -value, -is_anomaly)
-df_plot_residual <- rename(df_plot_residual, 时间 = timestamp)
+df_plot_residual_long <- df_with_outs |>
+  mutate(剩余项 = rem, 重估后的剩余项 = resid) |>
+  select(-value, -is_anomaly) |>
+  rename(时间 = timestamp) |>
+  pivot_longer(
+    cols = c("剩余项", "重估后的剩余项"),
+    names_to = "剩余/重估后的剩余项",
+    values_to = "电力需求(MW)"
+  )
 
-df_plot_residual_long <- pivot_longer(
-  df_plot_residual,
-  cols = c("剩余项", "重估后的剩余项"),
-  names_to = "剩余/重估后的剩余项",
-  values_to = "电力需求(MW)"
-)
-
-p_rem_resid <- ggplot(
-  df_plot_residual_long,
-  aes(x = 时间, y = `电力需求(MW)`, color = `剩余/重估后的剩余项`)
-) +
+p_rem_resid <- ggplot(df_plot_residual_long,
+                      aes(x = 时间, y = `电力需求(MW)`, color = `剩余/重估后的剩余项`)) +
   geom_line() +
   labs(x = "时间", y = "电力需求(MW)") +
-  scale_color_manual(values = c(
-    "剩余项" = "#C0C0C0",
-    "重估后的剩余项" = "#555555"
-  ))
-ggsave(paste0(asset_dir, "example-taylor-rem-resid.png"),
-  plot = p_rem_resid, width = 8, height = 4
+  scale_color_manual(values = c("剩余项" = "#C0C0C0", "重估后的剩余项" = "#555555"))
+ggsave(
+  paste0(asset_dir, "example-taylor-rem-resid.png"),
+  plot = p_rem_resid,
+  width = 8,
+  height = 4
 )
 
 
@@ -180,9 +173,10 @@ outs_ind_all <- union(outs_ind_TSA, outs_ind_IQR)
 # ---------------------------------------------------------
 df_IO <- tibble(lambda1 = IO$lambda1, ind = IO$ind)
 df_AO <- tibble(lambda2 = AO$lambda2, ind = AO$ind)
-df_IO_AO <- full_join(df_IO, df_AO, by = join_by(ind == ind))
-df_IO_AO$labels <- auto_recognize_AO_IO(AO, IO)$labels
-df_IO_AO <- select(arrange(df_IO_AO, ind), ind, lambda1, lambda2, labels)
+df_IO_AO <- full_join(df_IO, df_AO, by = join_by(ind == ind)) |>
+  mutate(labels = auto_recognize_AO_IO(AO, IO)$labels) |>
+  arrange(ind) |>
+  select(ind, lambda1, lambda2, labels)
 readr::write_csv(df_IO_AO, file = paste0(asset_dir, "example_taylor_IO_AO_df.csv"))
 
 
@@ -196,25 +190,35 @@ print(df_metrics)
 
 # 10. Plot data: pollu vs TSA vs IQR vs union
 # ------------------------------------------
-df_plot_outs_labeled <- df_with_outs
-df_plot_outs_labeled$is_anomaly <- as.factor(df_plot_outs_labeled$is_anomaly)
-df_plot_outs_labeled$异常检测来源 <- rep("-", nrow(df_plot_outs_labeled))
-df_plot_outs_labeled$异常检测来源[outs_ind_TSA] <- "Chang"
-df_plot_outs_labeled$异常检测来源[outs_ind_IQR] <- "IQR"
-df_plot_outs_labeled$异常检测来源[intersect(outs_ind_TSA, outs_ind_IQR)] <- "Chang & IQR"
-df_plot_outs_labeled <- rename(df_plot_outs_labeled,
-  时间 = timestamp,
-  `电力需求(MW)` = value,
-  值类型 = is_anomaly
-)
-df_plot_outs_labeled <- mutate(df_plot_outs_labeled, 值类型 = ifelse(值类型 == 0, "正常", "异常"))
+df_plot_outs_labeled <- df_with_outs |>
+  mutate(
+    is_anomaly = as.factor(is_anomaly),
+    异常检测来源 = case_when(
+      row_number() %in% intersect(outs_ind_TSA, outs_ind_IQR) ~ "Chang & IQR",
+      row_number() %in% outs_ind_TSA ~ "Chang",
+      row_number() %in% outs_ind_IQR ~ "IQR",
+      TRUE ~ "-"
+    )
+  ) |>
+  rename(时间 = timestamp,
+         `电力需求(MW)` = value,
+         值类型 = is_anomaly) |>
+  mutate(值类型 = ifelse(值类型 == 0, "正常", "异常"))
 
 p_TSA_IQR <- ggplot(df_plot_outs_labeled, aes(x = 时间, y = `电力需求(MW)`)) +
   geom_point(mapping = aes(color = 值类型, shape = 异常检测来源), size = 2) +
   geom_line(linewidth = 0.4) +
   scale_color_manual(values = c("正常" = "black", "异常" = "orange")) +
-  scale_shape_manual(values = c("-" = 20, "Chang" = 0, "IQR" = 2, "Chang & IQR" = 14)) +
+  scale_shape_manual(values = c(
+    "-" = 20,
+    "Chang" = 0,
+    "IQR" = 2,
+    "Chang & IQR" = 14
+  )) +
   labs(y = "电力需求(MW)")
-ggsave(paste0(asset_dir, "example-taylor-TSA-IQR.png"),
-  plot = p_TSA_IQR, width = 8, height = 4
+ggsave(
+  paste0(asset_dir, "example-taylor-TSA-IQR.png"),
+  plot = p_TSA_IQR,
+  width = 8,
+  height = 4
 )
